eec71bd312e6fb081381fc1523b8d44fce06037b
yashvinthan <yashvinthanmoorthy@gmail.com>
2026-03-09 18:56:34 +0530

    chore: allow test.webistzu.online in vite allowedHosts

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,8 @@ export default defineConfig({
     plugins: [react()],
     server: {
         allowedHosts: [
-            'lending-phd-vessels-rebate.trycloudflare.com'
+            'lending-phd-vessels-rebate.trycloudflare.com',
+            'test.webistzu.online'
         ]
     }
 })
